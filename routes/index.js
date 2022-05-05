4f40ef7ead8ae95cf6be38d37ca557a55b1b6ad3
xwritecodex <xwritecodex@gmail.com>
2022-05-04 15:42:10 -1000

    made cards unselectable. can only visit Design Deck page if at least one card is selected. Issues: closes #32

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,7 @@ const Deck = require('../models/Deck');
 const Card = require('../models/Card');
 const CardTag = require('../models/CardTag');
 const DeckTag = require('../models/DeckTag');
+const e = require('express');
 
 
 
@@ -21,7 +22,6 @@ router.get('/', authenticateLogin, async function(req, res, next) {
       JSON.parse(currentCookie).forEach((id) => {
         if (card._id.valueOf() == id) {
           card.selected = true
-          // Might have to go back and just add a third cardDiv partial for already selected cards, unless I can find a way to pass in variables to partials somehow.
         }
       })
     })
@@ -35,36 +35,42 @@ router.get('/', authenticateLogin, async function(req, res, next) {
 router.get('/select/:id', authenticateLogin, restrictRoute, async function (req, res, next) {
   let currentCookie = req.cookies.selected_cards;
   let cards = await Card.find({}).lean();
-  if (currentCookie != undefined) {
-    cards.forEach((card) => {
-      JSON.parse(currentCookie).forEach((id) => {
-        if (card._id.valueOf() == id) {
-          card.selected = true
-        }
-      })
-    })
-  }
   let id = [req.params.id]
+
   if (currentCookie == undefined) {
     res.cookie('selected_cards', JSON.stringify(id));
   } else {
     let parsedCookie = JSON.parse(currentCookie);
-    parsedCookie.push(req.params.id)
-    console.log("SELECTED CARD IDs:", parsedCookie)
-    res.clearCookie('selected_cards')
-    res.cookie('selected_cards', JSON.stringify(parsedCookie))
-  }
-  if (currentCookie != undefined) {
-    cards.forEach((card) => {
-      JSON.parse(currentCookie).forEach((id) => {
-        if (card._id.valueOf() == id) {
-          card.selected = true
+    let unselect = false
+
+    parsedCookie.forEach((id) => {
+      console.log(id)
+      console.log(req.params.id)
+      if (id == req.params.id) {
+        unselect = true
+      }
+    })
+
+    if (unselect == false) {
+      parsedCookie.push(req.params.id)
+      console.log("SELECTED CARD IDs:", parsedCookie)
+      res.clearCookie('selected_cards')
+      res.cookie('selected_cards', JSON.stringify(parsedCookie))
+    } else {
+      let cookie = parsedCookie
+      cookie.forEach((id, i) => {
+        if (req.params.id == id) {
+          parsedCookie.splice(i,1)
+        }
+        res.clearCookie('selected_cards')
+        if (parsedCookie.length > 0) {
+          res.cookie('selected_cards', JSON.stringify(cookie))
+          console.log("UNSELECTED CARD IDs:", parsedCookie)
         }
       })
-    })
+    }
   }
-  // let messages = [];
-  // res.render('index', { cards: cards, decks: decks, loggedIn: res.authenticate, username: res.username, messages: messages });
+
   res.redirect('/')
 })
 
