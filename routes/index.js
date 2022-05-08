0da2fe8076f1f02c9df8b2e1fc2c444bb1a323d6
xwritecodex <xwritecodex@gmail.com>
2022-05-07 15:22:28 -1000

    make ratings for cards and decks by clicking on appropriate stars Issues: closes #56

--- routes/index.js
+++ routes/index.js
@@ -54,46 +54,46 @@ router.get('/', authenticateLogin, async function(req, res, next) {
     let averageRating = (card.ratings.reduce(function (a, b) { return +a + +b }, 0) / card.ratings.length)
     if (averageRating > 0 && averageRating < 1.5) {
       card.stars = `
-      <i class='fa fa-star' style='color:#f3da35'></i>
-      <i class='fa fa-star'></i>
-      <i class='fa fa-star'></i>
-      <i class='fa fa-star'></i>
-      <i class='fa fa-star'></i>`
+      <a href='/rate/card/${card._id}/1' class="ratingLink"><i class='fa fa-star checked'></i></a>
+      <a href='/rate/card/${card._id}/2' class="ratingLink"><i class='fa fa-star'></i></a>
+      <a href='/rate/card/${card._id}/3' class="ratingLink"><i class='fa fa-star'></i></a>
+      <a href='/rate/card/${card._id}/4' class="ratingLink"><i class='fa fa-star'></i></a>
+      <a href='/rate/card/${card._id}/5' class="ratingLink"><i class='fa fa-star'></i></a>`
     } else if (averageRating >= 1.5 && averageRating < 2.5) {
       card.stars = `
-      <i class='fa fa-star' style='color:#f3da35'></i>
-      <i class='fa fa-star' style='color:#f3da35'></i>
-      <i class='fa fa-star'></i>
-      <i class='fa fa-star'></i>
-      <i class='fa fa-star'></i>`
+      <a href='/rate/card/${card._id}/1' class="ratingLink"><i class='fa fa-star checked'></i></a>
+      <a href='/rate/card/${card._id}/2' class="ratingLink"><i class='fa fa-star checked'></i></a>
+      <a href='/rate/card/${card._id}/3' class="ratingLink"><i class='fa fa-star'></i></a>
+      <a href='/rate/card/${card._id}/4' class="ratingLink"><i class='fa fa-star'></i></a>
+      <a href='/rate/card/${card._id}/5' class="ratingLink"><i class='fa fa-star'></i></a>`
     } else if (averageRating >= 2.5 && averageRating < 3.5) {
       card.stars = `
-      <i class='fa fa-star' style='color:#f3da35'></i>
-      <i class='fa fa-star' style='color:#f3da35'></i>
-      <i class='fa fa-star' style='color:#f3da35'></i>
-      <i class='fa fa-star'></i>
-      <i class='fa fa-star'></i>`
+      <a href='/rate/card/${card._id}/1' class="ratingLink"><i class='fa fa-star checked'></i></a>
+      <a href='/rate/card/${card._id}/2' class="ratingLink"><i class='fa fa-star checked'></i></a>
+      <a href='/rate/card/${card._id}/3' class="ratingLink"><i class='fa fa-star checked'></i></a>
+      <a href='/rate/card/${card._id}/4' class="ratingLink"><i class='fa fa-star'></i></a>
+      <a href='/rate/card/${card._id}/5' class="ratingLink"><i class='fa fa-star'></i></a>`
     } else if (averageRating >= 3.5 && averageRating < 4.5) {
       card.stars = `
-      <i class='fa fa-star' style='color:#f3da35'></i>
-      <i class='fa fa-star' style='color:#f3da35'></i>
-      <i class='fa fa-star' style='color:#f3da35'></i>
-      <i class='fa fa-star' style='color:#f3da35'></i>
-      <i class='fa fa-star'></i>`
+      <a href='/rate/card/${card._id}/1' class="ratingLink"><i class='fa fa-star checked'></i></a>
+      <a href='/rate/card/${card._id}/2' class="ratingLink"><i class='fa fa-star checked'></i></a>
+      <a href='/rate/card/${card._id}/3' class="ratingLink"><i class='fa fa-star checked'></i></a>
+      <a href='/rate/card/${card._id}/4' class="ratingLink"><i class='fa fa-star checked'></i></a>
+      <a href='/rate/card/${card._id}/5' class="ratingLink"><i class='fa fa-star'></i></a>`
     } else if (averageRating >= 4.5) {
       card.stars = `
-      <i class='fa fa-star' style='color:#f3da35'></i>
-      <i class='fa fa-star' style='color:#f3da35'></i>
-      <i class='fa fa-star' style='color:#f3da35'></i>
-      <i class='fa fa-star' style='color:#f3da35'></i>
-      <i class='fa fa-star' style='color:#f3da35'></i>`
+      <a href='/rate/card/${card._id}/1' class="ratingLink"><i class='fa fa-star checked'></i></a>
+      <a href='/rate/card/${card._id}/2' class="ratingLink"><i class='fa fa-star checked'></i></a>
+      <a href='/rate/card/${card._id}/3' class="ratingLink"><i class='fa fa-star checked'></i></a>
+      <a href='/rate/card/${card._id}/4' class="ratingLink"><i class='fa fa-star checked'></i></a>
+      <a href='/rate/card/${card._id}/5' class="ratingLink"><i class='fa fa-star checked'></i></a>`
     } else {
       card.stars = `
-      <i class='fa fa-star'></i>
-      <i class='fa fa-star'></i>
-      <i class='fa fa-star'></i>
-      <i class='fa fa-star'></i>
-      <i class='fa fa-star'></i>`
+      <a href='/rate/card/${card._id}/1' class="ratingLink"><i class='fa fa-star'></i></a>
+      <a href='/rate/card/${card._id}/2' class="ratingLink"><i class='fa fa-star'></i></a>
+      <a href='/rate/card/${card._id}/3' class="ratingLink"><i class='fa fa-star'></i></a>
+      <a href='/rate/card/${card._id}/4' class="ratingLink"><i class='fa fa-star'></i></a>
+      <a href='/rate/card/${card._id}/5' class="ratingLink"><i class='fa fa-star'></i></a>`
     }
   })
 
@@ -102,46 +102,46 @@ router.get('/', authenticateLogin, async function(req, res, next) {
     let averageRating = (deck.ratings.reduce(function (a, b) { return +a + +b }, 0) / deck.ratings.length)
     if (averageRating > 0 && averageRating < 1.5) {
       deck.stars = `
-      <i class='fa fa-star' style='color:#f3da35'></i>
-      <i class='fa fa-star'></i>
-      <i class='fa fa-star'></i>
-      <i class='fa fa-star'></i>
-      <i class='fa fa-star'></i>`
+      <a href='/rate/deck/${deck._id}/1' class="ratingLink"><i class='fa fa-star checked'></i></a>
+      <a href='/rate/deck/${deck._id}/2' class="ratingLink"><i class='fa fa-star'></i></a>
+      <a href='/rate/deck/${deck._id}/3' class="ratingLink"><i class='fa fa-star'></i></a>
+      <a href='/rate/deck/${deck._id}/4' class="ratingLink"><i class='fa fa-star'></i></a>
+      <a href='/rate/deck/${deck._id}/5' class="ratingLink"><i class='fa fa-star'></i></a>`
     } else if (averageRating >= 1.5 && averageRating < 2.5) {
       deck.stars = `
-      <i class='fa fa-star' style='color:#f3da35'></i>
-      <i class='fa fa-star' style='color:#f3da35'></i>
-      <i class='fa fa-star'></i>
-      <i class='fa fa-star'></i>
-      <i class='fa fa-star'></i>`
+      <a href='/rate/deck/${deck._id}/1' class="ratingLink"><i class='fa fa-star checked'></i></a>
+      <a href='/rate/deck/${deck._id}/2' class="ratingLink"><i class='fa fa-star checked'></i></a>
+      <a href='/rate/deck/${deck._id}/3' class="ratingLink"><i class='fa fa-star'></i></a>
+      <a href='/rate/deck/${deck._id}/4' class="ratingLink"><i class='fa fa-star'></i></a>
+      <a href='/rate/deck/${deck._id}/5' class="ratingLink"><i class='fa fa-star'></i></a>`
     } else if (averageRating >= 2.5 && averageRating < 3.5) {
       deck.stars = `
-      <i class='fa fa-star' style='color:#f3da35'></i>
-      <i class='fa fa-star' style='color:#f3da35'></i>
-      <i class='fa fa-star' style='color:#f3da35'></i>
-      <i class='fa fa-star'></i>
-      <i class='fa fa-star'></i>`
+      <a href='/rate/deck/${deck._id}/1' class="ratingLink"><i class='fa fa-star checked'></i></a>
+      <a href='/rate/deck/${deck._id}/2' class="ratingLink"><i class='fa fa-star checked'></i></a>
+      <a href='/rate/deck/${deck._id}/3' class="ratingLink"><i class='fa fa-star checked'></i></a>
+      <a href='/rate/deck/${deck._id}/4' class="ratingLink"><i class='fa fa-star'></i></a>
+      <a href='/rate/deck/${deck._id}/5' class="ratingLink"><i class='fa fa-star'></i></a>`
     } else if (averageRating >= 3.5 && averageRating < 4.5) {
       deck.stars = `
-      <i class='fa fa-star' style='color:#f3da35'></i>
-      <i class='fa fa-star' style='color:#f3da35'></i>
-      <i class='fa fa-star' style='color:#f3da35'></i>
-      <i class='fa fa-star' style='color:#f3da35'></i>
-      <i class='fa fa-star'></i>`
+      <a href='/rate/deck/${deck._id}/1' class="ratingLink"><i class='fa fa-star checked'></i></a>
+      <a href='/rate/deck/${deck._id}/2' class="ratingLink"><i class='fa fa-star checked'></i></a>
+      <a href='/rate/deck/${deck._id}/3' class="ratingLink"><i class='fa fa-star checked'></i></a>
+      <a href='/rate/deck/${deck._id}/4' class="ratingLink"><i class='fa fa-star checked'></i></a>
+      <a href='/rate/deck/${deck._id}/5' class="ratingLink"><i class='fa fa-star'></i></a>`
     } else if (averageRating >= 4.5) {
       deck.stars = `
-      <i class='fa fa-star' style='color:#f3da35'></i>
-      <i class='fa fa-star' style='color:#f3da35'></i>
-      <i class='fa fa-star' style='color:#f3da35'></i>
-      <i class='fa fa-star' style='color:#f3da35'></i>
-      <i class='fa fa-star' style='color:#f3da35'></i>`
+      <a href='/rate/deck/${deck._id}/1' class="ratingLink"><i class='fa fa-star checked'></i></a>
+      <a href='/rate/deck/${deck._id}/2' class="ratingLink"><i class='fa fa-star checked'></i></a>
+      <a href='/rate/deck/${deck._id}/3' class="ratingLink"><i class='fa fa-star checked'></i></a>
+      <a href='/rate/deck/${deck._id}/4' class="ratingLink"><i class='fa fa-star checked'></i></a>
+      <a href='/rate/deck/${deck._id}/5' class="ratingLink"><i class='fa fa-star checked'></i></a>`
     } else {
       deck.stars = `
-      <i class='fa fa-star'></i>
-      <i class='fa fa-star'></i>
-      <i class='fa fa-star'></i>
-      <i class='fa fa-star'></i>
-      <i class='fa fa-star'></i>`
+      <a href='/rate/deck/${deck._id}/1' class="ratingLink"><i class='fa fa-star'></i></a>
+      <a href='/rate/deck/${deck._id}/2' class="ratingLink"><i class='fa fa-star'></i></a>
+      <a href='/rate/deck/${deck._id}/3' class="ratingLink"><i class='fa fa-star'></i></a>
+      <a href='/rate/deck/${deck._id}/4' class="ratingLink"><i class='fa fa-star'></i></a>
+      <a href='/rate/deck/${deck._id}/5' class="ratingLink"><i class='fa fa-star'></i></a>`
     }
   })
 
