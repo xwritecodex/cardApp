402c9011f183d6caeb52a3bf5d9bd9eeda7860e6
xwritecodex <xwritecodex@gmail.com>
2022-05-08 19:30:49 -1000

    fix search autofill not working on some pages Issues: closes #71

--- routes/users.js
+++ routes/users.js
@@ -1,39 +1,90 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/User');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const Deck = require('../models/Deck');
+const Card = require('../models/Card');
+const CardTag = require('../models/CardTag');
+const DeckTag = require('../models/DeckTag');
+const User = require('../models/User');
 
 
 // GET LOGIN USER PAGE
 
-router.get('/login', function(req, res, next) {
-  res.render('login');
-});
+router.get('/login', async function(req, res, next) {
+  // IMPLEMENT AUTOFILL FOR QUERY
+  let searchables = []
+  let cardAutofill = await Card.find({}).lean()
+  cardAutofill.forEach((card) => {
+    card.cardName.split(' ').forEach((word) => {
+      searchables.push(word.toLowerCase())
+    })
+  })
+  let deckAutofill = await Deck.find({}).lean()
+  deckAutofill.forEach((deck) => {
+    deck.deckName.split(' ').forEach((word) => {
+      searchables.push(word.toLowerCase())
+    })
+  })
+  let taggedCardAutofill = await CardTag.find({}).lean()
+  taggedCardAutofill.forEach((tag) => {
+    searchables.push(tag.tag.toLowerCase())
+  })
+  let taggedDeckAutofill = await DeckTag.find({}).lean()
+  taggedDeckAutofill.forEach((tag) => {
+    searchables.push(tag.tag.toLowerCase())
+  })
+  searchables = Array.from(new Set(searchables));
+  res.render('login', { searchables: searchables });
+})
 
 // POST USER LOGIN
 
 router.post('/login', async function(req, res) {
-  const {username, password} = req.body;
+  // IMPLEMENT AUTOFILL FOR QUERY
+  let searchables = []
+  let cardAutofill = await Card.find({}).lean()
+  cardAutofill.forEach((card) => {
+    card.cardName.split(' ').forEach((word) => {
+      searchables.push(word.toLowerCase())
+    })
+  })
+  let deckAutofill = await Deck.find({}).lean()
+  deckAutofill.forEach((deck) => {
+    deck.deckName.split(' ').forEach((word) => {
+      searchables.push(word.toLowerCase())
+    })
+  })
+  let taggedCardAutofill = await CardTag.find({}).lean()
+  taggedCardAutofill.forEach((tag) => {
+    searchables.push(tag.tag.toLowerCase())
+  })
+  let taggedDeckAutofill = await DeckTag.find({}).lean()
+  taggedDeckAutofill.forEach((tag) => {
+    searchables.push(tag.tag.toLowerCase())
+  })
+  searchables = Array.from(new Set(searchables));
 
+
+  const {username, password} = req.body;
   let user;
   let match;
 
   if (username != '' && password != '') {
     if (username.length < 5 ) {
-      return res.render('login', { messages: ["Username must be at least 5 aplhanumeric chars in length."] });
+      return res.render('login', { messages: ["Username must be at least 5 aplhanumeric chars in length."], searchables: searchables });
     } else if (password.length < 8 || !(/^[a-zA-Z\d\-_]+$/.test(password))) {
-      return res.render('login', { messages: ["Password must be at least 8 aplhanumeric chars in length."] });
+      return res.render('login', { messages: ["Password must be at least 8 aplhanumeric chars in length."], searchables: searchables });
     } else {
       user = await User.findOne({username: username});
       if (user != null) {
         match = await bcrypt.compare(password, user.password);
       } else {
-        return res.render('login', { messages: ["User not found."] });
+        return res.render('login', { messages: ["User not found."], searchables: searchables });
       }
     }
   } else {
-    return res.render('login', { messages: ["Please fill in all fields!"] });
+    return res.render('login', { messages: ["Please fill in all fields!"], searchables: searchables });
   }
 
   if (match) {
@@ -45,28 +96,77 @@ router.post('/login', async function(req, res) {
     res.redirect('/');
 
   } else {
-    res.render('login', { messages: ["Invalid password."] });
+    res.render('login', { messages: ["Invalid password."], searchables: searchables });
   }
 })
 
 
 // GET REGISTER USER PAGE
 
-router.get('/register', function(req, res, next) {
-  res.render('register');
+router.get('/register', async function(req, res, next) {
+  // IMPLEMENT AUTOFILL FOR QUERY
+  let searchables = []
+  let cardAutofill = await Card.find({}).lean()
+  cardAutofill.forEach((card) => {
+    card.cardName.split(' ').forEach((word) => {
+      searchables.push(word.toLowerCase())
+    })
+  })
+  let deckAutofill = await Deck.find({}).lean()
+  deckAutofill.forEach((deck) => {
+    deck.deckName.split(' ').forEach((word) => {
+      searchables.push(word.toLowerCase())
+    })
+  })
+  let taggedCardAutofill = await CardTag.find({}).lean()
+  taggedCardAutofill.forEach((tag) => {
+    searchables.push(tag.tag.toLowerCase())
+  })
+  let taggedDeckAutofill = await DeckTag.find({}).lean()
+  taggedDeckAutofill.forEach((tag) => {
+    searchables.push(tag.tag.toLowerCase())
+  })
+  searchables = Array.from(new Set(searchables));
+
+  res.render('register',{ searchables: searchables });
 });
 
 // POST NEW USER
 
-router.post('/register', function(req, res, next) {
+router.post('/register', async function(req, res, next) {
+  // IMPLEMENT AUTOFILL FOR QUERY
+  let searchables = []
+  let cardAutofill = await Card.find({}).lean()
+  cardAutofill.forEach((card) => {
+    card.cardName.split(' ').forEach((word) => {
+      searchables.push(word.toLowerCase())
+    })
+  })
+  let deckAutofill = await Deck.find({}).lean()
+  deckAutofill.forEach((deck) => {
+    deck.deckName.split(' ').forEach((word) => {
+      searchables.push(word.toLowerCase())
+    })
+  })
+  let taggedCardAutofill = await CardTag.find({}).lean()
+  taggedCardAutofill.forEach((tag) => {
+    searchables.push(tag.tag.toLowerCase())
+  })
+  let taggedDeckAutofill = await DeckTag.find({}).lean()
+  taggedDeckAutofill.forEach((tag) => {
+    searchables.push(tag.tag.toLowerCase())
+  })
+  searchables = Array.from(new Set(searchables));
+
+
   const {username, userImageUrl, password, repeatPassword} = req.body;
 
   if (username != '' && password != '' && repeatPassword != '') {
     if (password != repeatPassword) {
-      return res.render('register', { messages: ["Password does not match re-password!"] });
+      return res.render('register', { messages: ["Password does not match re-password!"], searchables: searchables });
     } else {
       if (password.length < 8 || !(/^[a-z\d\-_]+$/.test(password))) {
-        return res.render('register', { messages: ["Password must be at least 8 aplhanumeric chars in length."] });
+        return res.render('register', { messages: ["Password must be at least 8 aplhanumeric chars in length."], searchables: searchables });
       } else {
         const salt = bcrypt.genSaltSync(+process.env.SALT);
         const hash = bcrypt.hashSync(password, salt);
@@ -83,15 +183,15 @@ router.post('/register', function(req, res, next) {
                 console.log(errMsg.username.properties.message);
                 messages.push(errMsg.username.properties.message);
             };
-            return res.render('register', { messages: messages });
+            return res.render('register', { messages: messages, searchables: searchables });
           } else {
-            return res.redirect('/users/login');
+            return res.render('login',{ searchables: searchables });
           };
         });
       };
     };
   } else {
-    res.render('register', { messages: ["Please fill in all fields!"] });
+    res.render('register', { messages: ["Please fill in all fields!"], searchables: searchables });
   }
 });
 
